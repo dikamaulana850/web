9ff85f2a72285e4a26fc59b6401071b676ff7c30
dikaa <dika28455@gmail.com>
2026-04-24 09:22:34 +0700

    Upload GitHub pertamaku

---
+++ data.js
@@ -0,0 +1,20 @@
+const databaseProyek = {
+    "sistem_post": {
+        judul: "Sistem Post untuk PT. Enak-Enak",
+        keahlian: ["HTML", "CSS", "JavaScript", "PHP", "MySQL"],
+        gambar: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTzC1PnAgEzf3bepO_ZDEaNzQmX6qNDZk0MSA&s",
+        latar: "Ini adalah proyek sistem post yang saya buat untuk PT. Enak-Enak, sebuah perusahaan makanan cepat saji. Sistem ini dirancang untuk memudahkan manajemen konten dan interaksi dengan pelanggan melalui platform digital.",
+        tantangan: "Kesulitan utama dalam proyek ini adalah mengintegrasikan berbagai fitur seperti manajemen konten, sistem komentar, dan analitik pengguna dalam satu platform yang mudah digunakan. Selain itu, memastikan keamanan data pengguna juga menjadi prioritas utama.",
+        pelajaran: "saya belajar banyak tentang pengembangan web full-stack, termasuk bagaimana merancang database yang efisien, mengimplementasikan fitur interaktif dengan JavaScript, dan memastikan keamanan aplikasi web. Proyek ini juga meningkatkan kemampuan saya dalam bekerja dengan tim dan berkomunikasi dengan klien untuk memahami kebutuhan mereka.",
+        link: ""
+    },
+    "sistem": {
+        judul: "Sistem Post untuk PT. Mantul",
+        keahlian: ["HTML", "CSS", "JavaScript", "PHP", "MySQL"],
+        gambar: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSA59CI0epZs4Htld_73pR1KPXnxnEtr0Igcw&s",
+        latar: "ini adalah proyek sistem post yang saya buat untuk PT. Mantul, sebuah perusahaan becak balap. Sistem ini dirancang untuk memudahkan manajemen konten dan interaksi dengan pelanggan melalui platform digital.",
+        tantangan: "kebutuhan dari PT-nya cukup kompleks, karena mereka ingin fitur yang memungkinkan pelanggan untuk memesan becak balap secara online, serta sistem manajemen untuk mengatur jadwal dan rute. Selain itu, memastikan keamanan data pengguna juga menjadi prioritas utama.",
+        pelajaran: "saya mendapatkan banyak pengalaman dalam mengembangkan aplikasi web yang kompleks, termasuk bagaimana merancang database yang efisien, mengimplementasikan fitur interaktif dengan JavaScript, dan memastikan keamanan aplikasi web. Proyek ini juga meningkatkan kemampuan saya dalam bekerja dengan tim dan berkomunikasi dengan klien untuk memahami kebutuhan mereka.",
+        link: ""
+    },
+};
